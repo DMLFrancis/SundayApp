81823047c9e09c79a2a037ce540e8c089ed3eee2
DMLFrancis <dml.francis@gmail.com>
2024-04-07 12:44:13 +0100

    6

---
+++ zap_scan.py
@@ -0,0 +1,14 @@
+# Import necessary ZAP libraries or modules
+from zapv2 import ZAPv2
+
+# Configure ZAP spider options
+spider_options = {
+    'target': 'http://localhost:8080',
+    'maxChildren': 5
+}
+
+# Initialize ZAP API client
+zap = ZAPv2()
+
+# Execute spider scan with configured options
+zap.spider.scan_as_user('zap_user', **spider_options)
